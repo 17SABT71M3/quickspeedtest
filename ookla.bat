3b152957e503732443b7a93936b933c5d85d641d
ITS0ABOUT0TIME <117502862+ITS0ABOUT0TIME@users.noreply.github.com>
2023-09-11 23:06:49 +0530

    Update ookla.bat

--- ookla.bat
+++ ookla.bat
@@ -9,8 +9,8 @@ REM -------------PLEASE REPLACE THE PATH BELOW
 .\ookla-speedtest-1.2.0-win64\speedtest.exe & REM------------------------PLEASE REPLACE THE PATH HERE EXCLUDING "& REM"----------------  IF THERE ARE SPACES USE DOUBLE-QUOTES.
 REM ---PLEASE REPLACE THE PATH ABOVE
 set /a errors=%errorlevel%
-if %errors%==3 cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & PAUSE >NUl
-if %errors%==9009 cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & PAUSE >NUl
+if %errors%==3 title no go &cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & PAUSE >NUl
+if %errors%==9009 title no go & cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & PAUSE >NUl
 REM :skip
 REM if defined jsons if %jsons%==Yes powershell -c "$jsonData = Get-Content -Raw -Path 'data.json' | ConvertFrom-Json; $uploadSpeed = $jsonData.upload.bandwidth; $downloadSpeed = ($jsonData.download.bandwidth * 8) / ( 1 * 1000000);$uploadSpeed = ($jsonData.upload.bandwidth * 8) / ( 1 * 1000000); $timestamp = $jsonData.timestamp;Write-Host \"$downloadSpeed\"
 
