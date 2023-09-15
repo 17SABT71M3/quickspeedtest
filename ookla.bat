ef972feb53048d54d1ffb79a721f7448e32d3037
ITS0ABOUT0TIME <117502862+ITS0ABOUT0TIME@users.noreply.github.com>
2023-09-15 09:50:19 +0530

    Update ookla.bat

--- ookla.bat
+++ ookla.bat
@@ -1,16 +1,20 @@
 @echo off
+REM initialise clip-board
+echo. | clip
+if EXIST "ooklaJwVDfDwUYTm.txt" goto MOVE_TO_NEXT
 echo.I appreciate your Patience in the usage of the Non-malicious Speed testing kit, >"ooklaJwVDfDwUYTm.txt"
-echo.To avoid clutter, the file ookla.bat (out of 2 files) is made to be hidden. U can unhide it >>"ooklaJwVDfDwUYTm.txt"
-echo.from the command line using the command "attrib -H ookla.bat"  >>"ooklaJwVDfDwUYTm.txt"
-echo.This Internet Speed testing kit has 2 files. >>"ooklaJwVDfDwUYTm.txt"
+echo.It is an Internet Speed Testing Kit to copy internet speed test Results >>"ooklaJwVDfDwUYTm.txt"
+echo.from the terminal. To avoid clutter, the file ookla.bat is made hidden. >>"ooklaJwVDfDwUYTm.txt"
+echo.U can unhide it if you wish to delete this internet speed testing kit. >>"ooklaJwVDfDwUYTm.txt"
 echo.Thank you, >>"ooklaJwVDfDwUYTm.txt"
 echo.Warm Regards, >>"ooklaJwVDfDwUYTm.txt"
+:MOVE_TO_NEXT
 attrib +H "%~fp0"
 
 REM
 REM The original bat file was written to capture the terminal output
 REM of the complete speed test and parse its json to get download / upload speed etc.
-REM for /f "tokens=*" %i in ('speedtest.exe -f json') do set jsondata=%i 
+REM for /f "tokens=*" %i in ('speedtest.exe -f json') do set jsondata=%i
 REM if "%1"=="skip" set jsons=Yes&goto skip
 REM set jsons=notbunty
 
@@ -23,13 +27,14 @@ REM -PLEASE REPLACE THE PATH BELOW use double quotes~
 REM -PLEASE REPLACE THE PATH ABOVE use double quotes~
 
 set errors=%errorlevel%
-if "%errors%"=="3" title ERROR 3 & Error 3 encountered & PAUSE >NUl
-if "%errors%"=="9009" title no go & cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & echo https://www.speedtest.net/apps/cli & echo Download the File For your Specific Operating System and Right click the exe file and select copy as path & PAUSE >NUl
+if "%errors%"=="2" powershell -c "$timae = Get-Date -Format \"dd-MM HH:mm\";write-host $timae suspected socket error" >>ooklaJwVDfDwUYTm.txt
+if "%errors%"=="3" powershell -c "$timae = Get-Date -Format \"dd-MM HH:mm\";write-host $timae error 3, that's all i know" >>ooklaJwVDfDwUYTm.txt
+if "%errors%"=="9009" title no go & cls & echo Open This Batch File in Notepad ^& Edit the Path to the Correct OOKLA exe & echo https://www.speedtest.net/apps/cli & echo Download the File For your Specific Operating System and Right click the exe file and select copy as path & powershell -c "$timae = Get-Date -Format \"dd-MM HH:mm\";write-host $timae Error 9009, the path to ookla-cli executable is WRONG, please EDIT ookla.bat using Notepad and change that" >>ooklaJwVDfDwUYTm.txt & PAUSE >NUl
 
 REM :skip
 REM if defined jsons if %jsons%==Yes powershell -c "$jsonData = Get-Content -Raw -Path 'data.json' | ConvertFrom-Json; $uploadSpeed = $jsonData.upload.bandwidth; $downloadSpeed = ($jsonData.download.bandwidth * 8) / ( 1 * 1000000);$uploadSpeed = ($jsonData.upload.bandwidth * 8) / ( 1 * 1000000); $timestamp = $jsonData.timestamp;Write-Host \"$downloadSpeed\"
-REM Write-Host 'Upload Speed: '\"$uploadSpeed mbps\"; Write-Host 'Download Speed: '\"$downloadSpeed\" mbps; Write-Host 'Timestamp: '$timestamp"  
-REM attrib +H data.json 
+REM Write-Host 'Upload Speed: '\"$uploadSpeed mbps\"; Write-Host 'Download Speed: '\"$downloadSpeed\" mbps; Write-Host 'Timestamp: '$timestamp"
+REM attrib +H data.json
 REM
 REM Thank yyou !!
 REM I appreciate your support!
